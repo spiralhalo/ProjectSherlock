d2ca796e13c442f832ac17b748236f4e3e8939a6
spiralhalo <re.nanashi95@gmail.com>
2018-09-15 06:28:05 +0700

    merge AFKMonitor into Tracker

--- src/xyz/spiralhalo/sherlock/Tracker.java
+++ src/xyz/spiralhalo/sherlock/Tracker.java
@@ -1,8 +1,15 @@
 package xyz.spiralhalo.sherlock;
 
+import lc.kra.system.keyboard.GlobalKeyboardHook;
+import lc.kra.system.keyboard.event.GlobalKeyAdapter;
+import lc.kra.system.keyboard.event.GlobalKeyEvent;
+import lc.kra.system.mouse.GlobalMouseHook;
+import lc.kra.system.mouse.event.GlobalMouseAdapter;
+import lc.kra.system.mouse.event.GlobalMouseEvent;
 import xyz.spiralhalo.sherlock.Main.Arg;
 import xyz.spiralhalo.sherlock.persist.project.Project;
 import xyz.spiralhalo.sherlock.persist.project.ProjectList;
+import xyz.spiralhalo.sherlock.persist.settings.UserConfig;
 import xyz.spiralhalo.sherlock.record.RealtimeRecordWriter;
 import xyz.spiralhalo.sherlock.util.Debug;
 import xyz.spiralhalo.sherlock.util.FormatUtil;
@@ -92,6 +99,41 @@ public class Tracker {
             Debug.logImportant("[Sandbox mode] Flushing buffer has been cancelled.");
         } else {
             buffer.flushBuffer();
+        }
+    }
+
+    private static class AFKMonitor {
+        private class MouseAdapter extends GlobalMouseAdapter {
+            @Override public void mousePressed(GlobalMouseEvent event) { logNow(); mousePressed = true; }
+            @Override public void mouseReleased(GlobalMouseEvent event) { logNow(); mousePressed = false; }
+            @Override public void mouseMoved(GlobalMouseEvent event) { logNow(); }
+            @Override public void mouseWheel(GlobalMouseEvent event) { logNow(); }
+        }
+
+        private class KeyAdapter extends GlobalKeyAdapter {
+            @Override public void keyPressed(GlobalKeyEvent event) { logNow(); keyPressed = true; }
+            @Override public void keyReleased(GlobalKeyEvent event) { logNow(); keyPressed = false; }
+        }
+
+        private void logNow(){ lastInput = System.currentTimeMillis(); }
+
+        private GlobalKeyboardHook keyboardHook;
+        private GlobalMouseHook mouseHook;
+
+        private long lastInput;
+        private boolean keyPressed;
+        private boolean mousePressed;
+
+        AFKMonitor() {
+            keyboardHook = new GlobalKeyboardHook(false);
+            mouseHook = new GlobalMouseHook(false);
+            keyboardHook.addKeyListener(new KeyAdapter());
+            mouseHook.addMouseListener(new MouseAdapter());
+        }
+
+        boolean isNotAFK() {
+            return keyPressed || mousePressed || (System.currentTimeMillis() - lastInput <
+                    UserConfig.getInt(UserConfig.UserNode.TRACKING, UserConfig.UserInt.AFK_TIMEOUT_SECOND) * 1000);
         }
     }
 }
