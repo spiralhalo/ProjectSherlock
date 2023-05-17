09185a61ae9cdf111dfa8b756ec45f3512db8175
spiralhalo <re.nanashi95@gmail.com>
2023-05-17 18:48:55 +0700

    Version bump to 6.4 for logging update

--- src/main/java/xyz/spiralhalo/sherlock/Main.java
+++ src/main/java/xyz/spiralhalo/sherlock/Main.java
@@ -36,8 +36,8 @@ import xyz.spiralhalo.sherlock.util.img.IconUtil;
 public class Main {
 
 	public static final int VERSION = 6;
-	public static final int VER_MINOR = 3;
-	public static final int VER_PATCH = 1;
+	public static final int VER_MINOR = 4;
+	public static final int VER_PATCH = 0;
 	public static final String VER_DESC = "";
 	public static final String APP_TITLE = String.format("Project Sherlock 2 version %d.%d.%d %s", VERSION, VER_MINOR, VER_PATCH, VER_DESC);
 	public static final String APP_NAME = "Project Sherlock 2";
